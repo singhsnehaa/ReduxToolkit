5c8e011dcf29444e864c5504ceaef42a1419b423
Sneha Singh <sneha.kumari@digital.datamatics.com>
2023-05-16 22:14:57 +0530

    deleted unwanted files

--- App.jsx
+++ App.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-import {Provider} from 'react-redux';
-import store from './src/redux/features/store'
-import TodoList from './src/screen/TodoList';
 
 const App = () => {
   return (
-    <Provider store={store}>
       <View style={styles.container}>
-        {/* <Text>Sneha</Text> */}
-        <TodoList/>
+        <Text>Sneha</Text>
       </View>
-    </Provider>
   );
 };
 
