a913791e54017c449c5cb3f49b85674d1cc4be71
Sneha Singh <sneha.kumari@digital.datamatics.com>
2023-05-16 22:56:22 +0530

    navigation related plugin has instaaled

--- App.jsx
+++ App.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 const App = () => {
   return (
-      <View style={styles.container}>
+    <NavigationContainer>{/* Rest of your app code */}
+     <View style={styles.container}>
         <Text>Sneha</Text>
       </View>
+    </NavigationContainer>
+     
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    // alignSelf: 'center',
-    // flex: 1,
-    // justifyContent: 'center',
-    marginTop:12,
+    alignSelf: 'center',
+    flex: 1,
+    justifyContent: 'center',
+    // marginTop:12,
     
   },
 });
